07af9e39059545c1cf5f5fee9b11b3cb5488cc86
Joshua Bradt <bradt@nscl.msu.edu>
2014-05-05 13:08:33 -0400

    Initial Commit

---
+++ GETConsolidate/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GETConsolidate
+//
+//  Created by Joshua Bradt on 5/5/14.
+//  Copyright (c) 2014 NSCL. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
