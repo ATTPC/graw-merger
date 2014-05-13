e463b8d1099daf7d2a3d7bdc30b6caf523601288
Joshua Bradt <bradt@nscl.msu.edu>
2014-05-13 14:26:37 -0400

    Added documentation to headers of PadLookupTable and GETDataFile classes.

--- GETConsolidate/GETDataFile.h
+++ GETConsolidate/GETDataFile.h
@@ -18,32 +18,57 @@
 
 class GETDataFile
 {
-private:
-    boost::filesystem::path filePath;
-    std::ifstream filestream;
+    /* This class represents the .GRAW file output by the DAQ. It contains a 
+     * C++ filestream to read in the file from disk, and it can then read
+     * individual frames from the file and provide them to another object, such
+     * as an instance of the GETFrame class.
+     */
     
-    uint8_t coboId;
-    uint8_t asadId;
-    
-    template<typename outType>
-    outType ExtractByteSwappedInt(std::vector<uint8_t>::iterator begin, std::vector<uint8_t>::iterator end);
-
 public:
     GETDataFile(const boost::filesystem::path& filePath_in);
+    /* The constructor takes as an argument the path of the directory which
+     * contains the .GRAW data files. It is assumed to be in the usual structure
+     * where there is a folder called "mm[n]" for each CoBo, where the [n] is 
+     * the CoBo number.
+     */
+    
     ~GETDataFile();
+    
+    /* Copy operators:
+     * These are disabled since copying a filestream is a bad idea.
+     */
     
     GETDataFile(const GETDataFile& orig) =delete;
     GETDataFile& operator=(const GETDataFile& other) =delete;
+    
+    // Move operators
     
     GETDataFile(GETDataFile&& orig);
     GETDataFile& operator=(GETDataFile&& orig);
     
     std::vector<uint8_t> GetNextRawFrame();
+    /* This function returns the next raw frame from the data file as a vector
+     * of integers. This can then be processed using, say, a GETFrame.
+     */
     
-    uint8_t GetFileCobo() const;
-    uint8_t GetFileAsad() const;
+    uint8_t GetFileCobo() const;    // Returns the CoBo # from the filename
+    uint8_t GetFileAsad() const;    // Returns the AsAd # from the filename
     
-    bool eof() const;
+    bool eof() const;               // Checks if the file pointer is at EOF
+    
+private:
+    boost::filesystem::path filePath;    // The location of the file
+    std::ifstream filestream;            // A filestream pointing to the file
+    
+    uint8_t coboId;                      // CoBo ID from the file name
+    uint8_t asadId;                      // AsAd ID from the file name
+    
+    template<typename outType>
+    outType ExtractByteSwappedInt(std::vector<uint8_t>::iterator begin, std::vector<uint8_t>::iterator end);
+    /* This function just byteswaps a provided integer. This is necessary since
+     * the .GRAW files are generated on PowerPC and are big-endian, while Intel 
+     * computers are little-endian.
+     */
 };
 
 #endif /* defined(__GETConsolidate__GETDataFile__) */
--- GETConsolidate/PadLookupTable.h
+++ GETConsolidate/PadLookupTable.h
@@ -15,15 +15,43 @@
 
 class PadLookupTable
 {
-private:
-    uint32_t CalculateHash(uint8_t cobo, uint8_t asad, uint8_t aget, uint8_t channel);
-    
-    std::map<uint32_t,uint16_t> table;
+    /* This class represents a lookup table that maps cobo, asad, aget, and 
+     * channel onto a pad number. This is dependent on the wiring of the ATTPC,
+     * and such a table can be generated using, for example, Igor Pro. For the
+     * purposes of this code, we assume that the table has already been made and
+     * is in an external file. The file name has to be passed to the constructor
+     * of this function, and it will be opened and read in.
+     */
     
 public:
     PadLookupTable(const std::string& path);
+    /* The constructor takes a string argument that specifies where on disk the 
+     * lookup table file is located. The file should be in csv format, with data
+     * in the order
+     *      cobo,asad,aget,channel,pad
+     * There should be no headers in the file.
+     *
+     * One big caveat: Igor's output files have classic Mac OS \r line endings. 
+     * This program assumes standard Unix \n line endings. This could easily be
+     * changed, but at the expense of using the standard. I've chosen to change 
+     * the line endings in the Igor file manually instead.
+     */
     
-    uint16_t FindPadNumber(uint8_t cobo, uint8_t asad, uint8_t aget, uint8_t channel);
+    uint16_t FindPadNumber(uint8_t cobo, uint8_t asad, uint8_t aget,
+                           uint8_t channel);
+    /* This function looks up a pad number in the table using the information 
+     * passed to the function.
+     */
+    
+private:
+    uint32_t CalculateHash(uint8_t cobo, uint8_t asad, uint8_t aget,
+                           uint8_t channel);
+    /* This is the hashing function for the hashtable used to store the pad 
+     * numbers. It simply concatenates cobo+asad+aget+channel since this is 
+     * unique for each pad.
+     */
+    
+    std::map<uint32_t,uint16_t> table;  // The hashtable, maps hash:pad#
 };
 
 #endif /* defined(__GETConsolidate__PadLookupTable__) */
