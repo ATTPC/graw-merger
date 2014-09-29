40ae34f43616af93027f2f87f62666c50e0f0a71
Joshua Bradt <bradt@nscl.msu.edu>
2014-09-29 08:46:03 -0400

    Added documentation to merger class.

--- get-manip/Merger.cpp
+++ get-manip/Merger.cpp
@@ -13,18 +13,23 @@ Merger::Merger()
 
 int Merger::AddFramesFromFileToIndex(const boost::filesystem::path& fpath)
 {
+    // Check if the file has already been read
     if (files.find(fpath.filename().string()) != files.end()) {
         throw Exceptions::File_Already_Read(fpath.filename().string());
     }
     
+    // Open the file
     std::shared_ptr<GRAWFile> file {new GRAWFile(fpath, std::ios::in)};
     
     int nFramesRead {0};
     
+    // Index the frames in the file
     while (!file->eof()) {
         try {
             size_t currentPos = file->filestream.tellg();
-            GRAWFrame frame {file->ReadRawFrame()};
+            
+            // Reading the frame will advance the file pointer
+            GRAWFrame frame {file->ReadRawFrame()};  // inefficient?
             
             FrameMetadata fm;
             fm.filePtr = file;
@@ -41,9 +46,13 @@ int Merger::AddFramesFromFileToIndex(const boost::filesystem::path& fpath)
         }
         catch (std::exception& e) {
             std::cout << "Exception: " << e.what() << std::endl;
-            throw e;
+            throw;
         }
     }
+    
+    // Put the file in the list of files we've already read. This also copies
+    // the shared pointer, which keeps the file around after this function
+    // returns.
     
     files.emplace(fpath.filename().string(),file);
     return nFramesRead;
@@ -56,21 +65,23 @@ void Merger::MergeByEvtId(const std::string &outfilename, PadLookupTable* lt)
     
     std::cout << "Beginning merge of " << mmap.size() << " frames." << std::endl;
     
-    // Find first event
+    // Find first event and number of events
     uint32_t firstEvtId = mmap.lower_bound(0)->first;
     uint32_t numEvts = mmap.rbegin()->first - firstEvtId;
     
     // Create events and append to file
     for (uint32_t currentEvtId = firstEvtId ; mmap.size() > 0; currentEvtId++ )
     {
-        // Find frames for this event
+        // Find frames for this event. equal_range() returns a pair containing
+        // iterators to the first and last frame with this event ID. If no frames
+        // are found, they'll both point to the next highest event ID.
         auto currentRange = mmap.equal_range(currentEvtId);
-        if (currentRange.first->first > currentEvtId) continue;
+        if (currentRange.first->first > currentEvtId) continue; // no frames found
         
         // Extract frames from files
         std::queue<GRAWFrame> frames;
         for (auto frameRefPtr = currentRange.first; frameRefPtr != currentRange.second; frameRefPtr++) {
-            auto& fref = frameRefPtr->second;
+            FrameMetadata& fref = frameRefPtr->second;
             fref.filePtr->filestream.seekg(fref.filePos);
             std::vector<uint8_t> rawFrame = fref.filePtr->ReadRawFrame();
             frames.push(GRAWFrame {rawFrame});
@@ -95,6 +106,10 @@ void Merger::MergeByEvtId(const std::string &outfilename, PadLookupTable* lt)
 
 void Merger::ShowProgress(int currEvt, int numEvt)
 {
+    // Produces a progress bar like this:
+    //  50% [==========          ] (50/100)
+    // The last part is the current evt / num evts
+    
     const int progWidth = 40;
     int pct = floor(float(currEvt) / float(numEvt) * 100);
     std::cout << '\r' << std::string(progWidth,' ') << '\r';
--- get-manip/Merger.h
+++ get-manip/Merger.h
@@ -23,29 +23,53 @@
 #include <string>
 #include <iostream>
 
+/** \brief GRAW file merger class
+ 
+ This class implements the actual file merging functionality. It receives GRAW file paths from the caller, opens them, indexes their contents by event ID, and merges events by event ID into an output file. 
+ 
+ The design of the merger was inspired by a similar program found in the cobo-frame-viewer code.
+ 
+ */
 class Merger
 {
 public:
+    //! \brief The default constructor. Does nothing.
     Merger();
+    
+    /** \brief Opens and indexes the given file.
+     
+     This function opens the file found at the given path and adds its contents to the frame index. It does not explicitly verify that the file exists and is valid, so this should be checked ahead of time.
+     
+     \returns The number of frames found in the file.
+     \throws A variety of exceptions since it uses the GRAWFrame class internally.
+     \throws Exceptions::File_Already_Read if the given file is already in the index.
+     
+     */
     int AddFramesFromFileToIndex(const boost::filesystem::path& fpath);
+    
+    /** \brief Merges the indexed events by common event ID.
+     
+     This function does the actual merging of the files. It looks through the map of events created by the AddFramesFromFileToIndex function and puts together frames that have the same event ID. The fixed pattern noise is subtracted from these events, and they are then written to a new file.
+     
+     */
     void MergeByEvtId(const std::string& outfilename, PadLookupTable* lt);
     
 private:
     struct FrameMetadata
     {
-        std::shared_ptr<GRAWFile> filePtr;
-        size_t filePos; // Position of the frame in the file
+        std::shared_ptr<GRAWFile> filePtr; // Pointer to the file that contains this frame
+        size_t filePos;                    // Position of the frame in the file
         uint32_t evtId;
         uint64_t evtTime;
     };
     
-    typedef std::multimap<uint32_t, FrameMetadata> MergingMap;
+    typedef std::multimap<uint32_t, FrameMetadata> MergingMap; // key = event ID
     MergingMap mmap;
     
+    // This map is for keeping track of what files we've already seen
     std::map<std::string, std::shared_ptr<GRAWFile>> files;
-    
-    FrameMetadata ParseRawFrame (const std::vector<uint8_t> raw);
-    
+
+    //! \brief Creates the progress bar in the terminal
     void ShowProgress(int currEvt, int numEvt);
 };
 
